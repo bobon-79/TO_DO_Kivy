4bc57b24e6c0d8f09d3ebea87e2f93b6da37921e
bobon <andreev.bobon@gmail.com>
2025-09-26 16:47:00 +0300

    Continued development to release.

--- screens/main_menu.py
+++ screens/main_menu.py
@@ -1,23 +1,21 @@
 """
 The main menu screen of the project
 """
-
-from kivy.properties import StringProperty
+from kivy.clock import Clock
+from kivy.properties import StringProperty, ColorProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
 
 from utils import *
 
 
-class MainMenu(BoxLayout,Screen):
+class MainMenu(BoxLayout, Screen):
     """
      Class for the main menu screen of the project
     """
     text_label = StringProperty(i18n.get_locale_text("main_menu", "title_label"))
-    icon = str(config.BASE_DIR / "assets" / "label_mainmenu.png")
-    color: list = [0.35, 0.88, 0.38, 1]
+    color = ColorProperty([0.35, 0.88, 0.38, 1])
+    img = chr(int(i18n.get_locale_text("main_menu", "img")[1:], 16))
 
-
-
-
-
+    def on_kv_post(self, base_widget):
+        Clock.schedule_once(lambda dt: print(self.ids.card_label.img))
