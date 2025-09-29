2a0bf3c785ff8340057a883d6ed370e4be62de22
bobon <andreev.bobon@gmail.com>
2025-09-29 10:22:46 +0300

    Continuation of the project construction.

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ Entry point of the project.
 """
 import sys
 
-from screens import *
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from kivy.core.window import Window
 
+from utils import config
+from screens import *
 
-class TO_DOApp(App):
-    #list_screens: list = [MainMenu()]
+
+class TO_DOApp(App, BoxLayout):
+    # list_screens: list = [MainMenu()]
+    UI = StringProperty(str(config.BASE_DIR / "assets/fonts/materialdesignicons-webfont.ttf"))
+    MDI = StringProperty(str(config.BASE_DIR / "assets/fonts/Wremena-Regular.ttf"))
 
     def on_start(self):
         if sys.platform.startswith("win"):
-            Window.size = (324, 720)  # ширина, высота
+            Window.size = (324, 720)
+            Window.clearcolor = 1, 1, 1, 1  # ширина, высота
 
     def build(self):
         pass
@@ -21,4 +28,3 @@ class TO_DOApp(App):
 
 if __name__ == '__main__':
     TO_DOApp().run()
-
--- screens/main_menu.py
+++ screens/main_menu.py
@@ -14,8 +14,13 @@ class MainMenu(BoxLayout, Screen):
      Class for the main menu screen of the project
     """
     text_label = StringProperty(i18n.get_locale_text("main_menu", "title_label"))
-    color = ColorProperty([0.35, 0.88, 0.38, 1])
+    blue = StringProperty(i18n.get_locale_text("app", "blue"))
     img = chr(int(i18n.get_locale_text("main_menu", "img")[1:], 16))
 
     def on_kv_post(self, base_widget):
-        Clock.schedule_once(lambda dt: print(self.ids.card_label.img))
+        """
+
+        :param base_widget:
+        """
+
+        Clock.schedule_once(lambda dt: print(self.ids.card_label.size))
